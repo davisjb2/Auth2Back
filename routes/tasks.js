b2e31c8611d856a7e74b6185901e4cc13ccca894
Brooke Tibbett <davisjb2@appstate.edu>
2019-10-17 15:08:44 -0400

    fix task update

--- routes/tasks.js
+++ routes/tasks.js
@@ -45,7 +45,7 @@ router.post('/update/:id', async (req, res) => {
         {
             await task.update(req.body)
         }
-        return res.status(200).send({ status: 200, result: undefined})
+        return res.status(200).send({ status: 200, result: task})
     } catch (e) {
         return res.status(200).send({ status: 500, result: undefined, error: e.message})
     }
